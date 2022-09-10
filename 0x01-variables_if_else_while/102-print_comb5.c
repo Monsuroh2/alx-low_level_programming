146701b56c8216743a87932aa6f047d3573e3911
Monsurat Raimi <raimimonsurat2@gmail.com>
2022-09-09 19:02:13 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,37 +7,25 @@
  */
 int main(void)
 {
-	int c, i, k, j;
+	int i, o;
 
-	for (c = 48; c <= 57; c++)
+	for (i = 0; i < 100; i++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (o = i + 1; o < 100; o++)
 		{
-			for (k = 48; k <= 57; k++)
-			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) && k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(k);
-						putchar(j);
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((o / 10) + '0');
+		putchar((o % 10) + '0');
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
+		if (i == 98 && o == 99)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
