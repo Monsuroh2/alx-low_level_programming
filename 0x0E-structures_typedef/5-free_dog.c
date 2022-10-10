645495f9597420533b3cb47df7e99804049509b3
Monsurat Raimi <raimimonsurat2@gmail.com>
2022-10-10 10:51:49 -0700

    dog.h

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees a dog
+ * @d: A dog struct
+ *
+ * Return: Null void
+ */
+
+void free_dog(dog_t *d)
+{
+	f (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
