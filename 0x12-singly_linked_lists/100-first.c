b50a49749bc48621d53bc80b0fe87306bb9d279a
Monsurat Raimi <raimimonsurat2@gmail.com>
2022-10-20 07:03:54 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,7 @@
  * Return: Null Void
  */
 
-void before_main(void) __attribute__((constructor));
-void before_main(void)
+void __attribute__ ((constructor)) beforemain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
